4bc7a4072bda689f2890981f4da050751deb2050
NANDAN R <nandansiddamma986@gmail.com>
2026-04-03 00:01:39 +0530

    Connected Railway DB

--- server.js
+++ server.js
@@ -19,15 +19,19 @@ app.use(express.static(path.join(__dirname, 'frontend')));
 
 // MySQL Connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect(err => {
-  if (err) throw err;
-  console.log('MySQL Connected');
+  if (err) {
+    console.error('DB Connection Failed:', err);
+  } else {
+    console.log('Railway MySQL Connected 🚀');
+  }
 });
 
 // ---------------------- MEDICINES ----------------------
