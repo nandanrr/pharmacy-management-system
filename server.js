84ce9c787be5a52b88f340b1d713d4674efe9490
NANDAN R <nandansiddamma986@gmail.com>
2026-04-03 10:30:25 +0530

    Fix MySQL auth using mysql2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
